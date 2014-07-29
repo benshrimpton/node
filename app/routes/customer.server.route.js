92b83d0d51f977cc0e40d11fba94f771576d8e2f
johnson ooi <ojh5065@gmail.com>
2014-07-29 17:08:23 -0400

    - added new routes

--- app/routes/customer.server.route.js
+++ app/routes/customer.server.route.js
@@ -43,6 +43,33 @@ module.exports = function(app){
         .route('/customer/profile')
         .get(Customer.hasAuthorization, Customer.profile);
 
+    /*
+    * Create an address for a customer;
+    * Retrieve the customer's addresses
+    * **/
+    app
+        .route('/customer/address')
+        .get(Customer.hasAuthorization, Customer.customerAddressList)
+        .post(Customer.hasAuthorization, Customer.createCustomerAddress);
+
+
+    /*
+    * Retrieve the specified address of the customer
+    * Update the specified address of the customer
+    * **/
+    app
+        .route('/customer/address/:addressId')
+        .get(Customer.hasAuthorization, Customer.customerAddressDetail)
+        .post(Customer.hasAuthorization, Customer.updateCustomerAddress);
+
+
+    /*
+    * Delete the specified user's account.
+    * **/
+    app
+        .route('/customer/address/delete/:addressId')
+        .get(Customer.hasAuthorization, Customer.removeCustomerAddress);
+
 
     /*
     * Customer Logout Page
